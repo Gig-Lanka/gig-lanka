68c93626a377c2fe7e1c9949186a967d5c3e64e6
Lahiru <workinglahiru@gmail.com>
2026-08-03 13:34:30 +0530

    GL-28: Wire helmet, cors, and morgan middleware and add health check endpoint

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,16 @@
 import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
+import healthRoutes from "./routes/health.routes.js";
 
 const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use("/api/health", healthRoutes);
 
 export default app;
