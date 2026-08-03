3ec0f852d648ede509a04e65672b0b3fe7bf0443
Lahiru <workinglahiru@gmail.com>
2026-08-03 13:51:26 +0530

    GL-30: Centralise environment config in config/env.js and add .env.example

--- server/src/middleware/errorHandler.js
+++ server/src/middleware/errorHandler.js
@@ -1,3 +1,5 @@
+import { env } from "../config/env.js";
+
 export const errorHandler = (err, req, res, next) => {
   console.error(err);
 
@@ -6,7 +8,7 @@ export const errorHandler = (err, req, res, next) => {
   res.status(status).json({
     error: {
       message: err.message || "Internal Server Error",
-      ...(process.env.NODE_ENV !== "production" && { stack: err.stack }),
+      ...(env.nodeEnv !== "production" && { stack: err.stack }),
     },
   });
 };
