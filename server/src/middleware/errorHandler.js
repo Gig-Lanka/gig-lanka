047447b431fe8153f56388ab77cd965ae18c274f
Anupa Prabhasara <info@anupa.lk>
2026-08-04 16:24:40 +0530

    GL-51 : Fix error handler

--- server/src/middleware/errorHandler.js
+++ server/src/middleware/errorHandler.js
@@ -1,7 +1,7 @@
-import { ApiError } from "../utils/ApiError.js";
-import { sendError } from "../utils/response.js";
+import { ApiError } from '../utils/ApiError.js';
+import { sendError } from '../utils/response.js';
 
-export const errorHandler = (err, req, res, next) => {
+export const errorHandler = (err, req, res, _next) => {
   const isApiError = err instanceof ApiError;
 
   if (!isApiError) {
@@ -9,8 +9,8 @@ export const errorHandler = (err, req, res, next) => {
   }
 
   const status = isApiError ? err.status : 500;
-  const code = isApiError ? err.code : "INTERNAL_ERROR";
-  const message = isApiError ? err.message : "Internal Server Error";
+  const code = isApiError ? err.code : 'INTERNAL_ERROR';
+  const message = isApiError ? err.message : 'Internal Server Error';
   const errors = isApiError ? err.errors : undefined;
 
   sendError(res, status, code, message, errors);
