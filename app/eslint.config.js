820aa42c7cbf4a6ad08b4e1d87ebd7044422111e
Anupa Prabhasara <info@anupa.lk>
2026-08-04 13:51:15 +0530

    GL-48 ESLint configured on frontend completely

---
+++ app/eslint.config.js
@@ -0,0 +1,42 @@
+const js = require('@eslint/js');
+const globals = require('globals');
+const reactPlugin = require('eslint-plugin-react');
+const reactHooksPlugin = require('eslint-plugin-react-hooks');
+const prettierConfig = require('eslint-config-prettier');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx}'],
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin,
+    },
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        __DEV__: 'readonly',
+      },
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+    rules: {
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+      'react-hooks/rules-of-hooks': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+    },
+  },
+  prettierConfig,
+  {
+    ignores: ['node_modules/**', '.expo/**', 'dist/**', 'web-build/**'],
+  },
+];
